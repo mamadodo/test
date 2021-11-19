4d7254872115ac057bc0214771729e75f2d3f82b
em_mnagai <m_nagai@entermotion.jp>
2021-11-19 13:07:38 +0900

    Add build folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+import sound from "./MusMus-JGL-017.mp3";
+const music = new Audio(sound);
+
+const onClickPlay = () => music.play();
+const onClickStop = () => music.pause();
+const onClickLoopTrue = () => {
+  music.loop = true;
+  console.log("ループあり");
+};
+const onClickLoopFalse = () => {
+  music.loop = false;
+  console.log("ループなし");
+};
+const onClickCurrentTime = () => (music.currentTime = 0);
+const onClickEnded = () => {
+  if (music.ended) {
+    alert("再生終了");
+  } else {
+    alert("再生前or再生中");
+  }
+};
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="wrap">
+        <button onClick={onClickStop}>停止</button>
+        <button onClick={onClickPlay}>再生</button>
+        <button onClick={onClickLoopTrue}>ループ再生ON</button>
+        <button onClick={onClickLoopFalse}>ループ再生OFF</button>
+        <button onClick={onClickCurrentTime}>最初へ</button>
+        <button onClick={onClickEnded}>再生終了確認</button>
+      </div>
+    </>
   );
-}
-
-export default App;
+};
